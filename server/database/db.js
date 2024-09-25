87a45b03a9d3330507f2b4737178df0bebd2e8d2
Akhil <akhilkum249@gmail.com>
2024-09-25 11:13:05 +0530

    connected to database:successfully

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,16 @@
+
+import dotenv from 'dotenv'
+dotenv.config()
 import mongoose from 'mongoose';
 
 const mongodb = process.env.MONGODBURL;
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(mongodb);
-        console.log("Connected to db");
+        await mongoose.connect(mongodb).then(()=>{
+            console.log(`Databse connected successfully!!`)
+        })
+       
     } catch (error) {
         console.log(`Database connection failed:`, error);
     }
